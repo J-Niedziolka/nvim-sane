4207597ea479992c5a018640cd62a7d20bccb99b
Jan Niedziolka <j.niedziolka@samsung.com>
2024-12-08 23:33:48 +0100

    merged work pc config with home workstation config

--- init.lua
+++ init.lua
@@ -3,5 +3,23 @@ require("opt")
 require("plugins")
 require("autocmds")
 
+<<<<<<< Updated upstream
 -- vim.cmd([[colorscheme evening]])
 vim.cmd(":source ~/.config/nvim/misc.vim")
+=======
+--vim.opt.clipboard = "xclip"
+vim.cmd([[
+	source ~/.config/nvim/misc.vim
+	set runtimepath^=~/.vim runtimepath+=~/.vim/after
+	let &packpath = &runtimepath
+	source ~/.vimrc
+
+
+" highlight all occurrances of word under cursor
+" augroup highlight_current_word
+"	set updatetime=300
+"	au!
+"au CursorHold * :exec 'match Search /\V\<' . expand('<cword>') . '\>/'
+" augroup END
+]])
+>>>>>>> Stashed changes
